0411afb324f749f7d8cb5a91c2ab6edf8627a51f
Maimai10808 <331686549@qq.com>
2025-07-26 00:02:59 +0800

    15. Question Model

--- ScrambledWords/ContentView.swift
+++ ScrambledWords/ContentView.swift
@@ -21,7 +21,32 @@ struct ContentView: View {
     @State var showFailure = false
     @State var score: Int = 0
     
-    let correctAnwer = "ORANGE"
+    var questions: [Question] = [
+        Question(image: "orange", scrambledletters: [
+            Letter(id: 0, text: "A"),
+            Letter(id: 1, text: "O"),
+            Letter(id: 2, text: "E"),
+            Letter(id: 3, text: "R"),
+            Letter(id: 4, text: "N"),
+            Letter(id: 5, text: "G")
+        ], Answer: "ORANGE"),
+        Question(image: "banana", scrambledletters: [
+            Letter(id: 0, text: "A"),
+            Letter(id: 1, text: "A"),
+            Letter(id: 2, text: "N"),
+            Letter(id: 3, text: "B"),
+            Letter(id: 4, text: "N"),
+            Letter(id: 5, text: "A")
+        ], Answer: "BANANA"),
+        Question(image: "apple", scrambledletters: [
+            Letter(id: 0, text: "A"),
+            Letter(id: 1, text: "L"),
+            Letter(id: 2, text: "E"),
+            Letter(id: 3, text: "P"),
+            Letter(id: 4, text: "P")
+        ], Answer: "APPLE")
+        
+    ]
     
     var body: some View {
             GeometryReader { proxy in
---
+++ ScrambledWords/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  ScrambledWords
+//
+//  Created by mac on 7/25/25.
+//
+
+import Foundation
+
+struct Question {
+    let image: String
+    var scrambledletters: [Letter]
+    let Answer: String
+}
