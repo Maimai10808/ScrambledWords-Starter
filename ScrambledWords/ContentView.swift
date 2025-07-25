d30e9250d920653ede9f0796a13d72fa40f2bfad
Maimai10808 <331686549@qq.com>
2025-07-25 11:29:12 +0800

    2. User Interface Part 1

--- ScrambledWords/ContentView.swift
+++ ScrambledWords/ContentView.swift
@@ -7,9 +7,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
+        
+        GeometryReader { proxy in
+            ZStack {
+                Color.backgroud
+                    .ignoresSafeArea()
+                
+                VStack {
+                    VStack {
+                        Image("orange")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                    }
+                    .frame(width: proxy.size.width * 0.9,height: proxy.size.width * 0.9)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.border, lineWidth: 2)
+                    }
+                    Text("Score 0")
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color.white)
+                }
+            }
         }
+        
     }
 }
 
