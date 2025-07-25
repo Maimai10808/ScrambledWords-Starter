22a3347c12bd10341ab2b0f06e2faa3e2f3ba9e6
Maimai10808 <331686549@qq.com>
2025-07-25 10:22:58 +0800

    1. init

---
+++ ScrambledWords/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ScrambledWords
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
